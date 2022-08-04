c7a00652ec483513c0d2a49edf0de2438d544199
Surajit <32619472+sdas1020@users.noreply.github.com>
2022-08-05 00:17:29 +0530

    new

--- app.py
+++ app.py
@@ -299,7 +299,7 @@ def main():
                             pass
                 with st.container():
                     if (get_title(data3, CAT3).split(":")[1].split("-")[0].strip() == "negative") and (get_title(pred, CAT6).split(":")[1].split("-")[0].strip() == "angry") and ((get_title(pred_, CAT7).split(":")[1].split("-")[0].strip()) == "disgust") :
-                        st.write("## Child is undergoing anxiety and stress. Please reach out to him or consult a BH specialist")
+                        st.write("## Child is undergoing anxiety and stress. Please reach out to him or consult a BH specialist.")
 
     elif website_menu == "Project description":
         import pandas as pd
